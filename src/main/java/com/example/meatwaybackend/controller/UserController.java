f623509e859bb326b3d02a96d2e7aeefb72b97af
RaketaBoom <77796518+RaketaBoom@users.noreply.github.com>
2025-03-24 22:47:19 +0400

    fix: review fix

--- src/main/java/com/example/meatwaybackend/controller/UserController.java
+++ src/main/java/com/example/meatwaybackend/controller/UserController.java
@@ -1,11 +1,24 @@
 package com.example.meatwaybackend.controller;
 
-import com.example.meatwaybackend.dto.user.*;
+import com.example.meatwaybackend.dto.user.CreatedUserResponse;
+import com.example.meatwaybackend.dto.user.UserCreateRequest;
+import com.example.meatwaybackend.dto.user.UserEditRequest;
+import com.example.meatwaybackend.dto.user.UserProfileResponse;
+import com.example.meatwaybackend.dto.user.UserProfilesResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
@@ -45,7 +58,7 @@ public class UserController {
             summary = "Создать пользователя по id",
             tags = {USER_CONTROLLER}
     )
-    public CreatedUserResponse createUser(@RequestBody UserCreateRequest userCreateRequest){
+    public CreatedUserResponse createUser(@RequestBody UserCreateRequest userCreateRequest) {
         //TODO business
         return null;
     }
@@ -67,7 +80,7 @@ public class UserController {
             summary = "Внести изменения в профиль пользователя",
             tags = {USER_CONTROLLER}
     )
-    public void deleteUser(@PathVariable long id){
+    public void deleteUser(@PathVariable long id) {
         //TODO business
     }
 }
